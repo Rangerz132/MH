3489f263f5c8d72b28d5a5f14ce59a1d4c5d7956
Anthony Trépanier <anthony.trepanier@hotmail.com>
2020-06-09 14:15:19 -0400

    - Adjust character animation - Set transition to animation - Create player states

---
+++ Assets/MH/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Movements")]
+    [SerializeField] private float movementSpeed = 3f;
+    private CharacterController characterController;
+    private Animator animator;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Player movement;
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        animator.SetFloat("InputX", moveX);
+        animator.SetFloat("InputY", moveZ);
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ; //direction and facing
+
+        characterController.Move(move * movementSpeed * Time.deltaTime);
+
+    }
+
+
+  
+
+
+
+}
---
+++ Assets/MH/Scripts/PlayerState.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+
+    private Animator animator;
+    public enum playerState {Unarmed, Armed};
+    public playerState stateValue;
+    public GameObject weapon;
+    public Transform weaponHolderHand;
+    public Transform weaponHolderBag;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        stateValue = playerState.Unarmed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectState();
+    }
+
+
+    private void DetectState() {
+
+        if (Input.GetButtonDown("SwitchArmState") || Input.GetKeyDown(KeyCode.Q)) {
+
+            animator.SetTrigger("SwitchArmState");
+
+            switch (stateValue) {
+
+                case playerState.Unarmed:
+
+                    Debug.Log("Unarmed");
+
+                    stateValue = playerState.Armed;
+                    animator.SetInteger("ArmStateID", 1);
+                    break;
+
+                case playerState.Armed:
+
+                    Debug.Log("armed");
+
+                    stateValue = playerState.Unarmed;
+                    animator.SetInteger("ArmStateID", 0);
+                    break;
+     
+            }
+        }   
+    }
+
+    public void ChangeWeaponOriginToHand() {
+        weapon.transform.SetParent(weaponHolderHand);
+    }
+
+
+    public void ChangeWeaponOriginToBag() {
+        weapon.transform.SetParent(weaponHolderBag);
+    }
+
+    public void testtest(Transform testObject) {
+
+        weapon.transform.SetParent(testObject);
+    }
+
+
+}
