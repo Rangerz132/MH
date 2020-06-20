45441dbb474b53f413affcc6466f98368f317436
Anthony Trépanier <anthony.trepanier@hotmail.com>
2020-06-20 11:17:05 -0400

    - Create fireball fx

---
+++ Assets/MH/Scripts/Camera/CameraInteractable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class CameraInteractable : MonoBehaviour
+{
+
+    public float maxDist = 10f;
+    public Color highlighColor;
+    public Color defaultColor;
+    public Material targetMat;
+    RaycastHit hit;
+
+
+    
+
+
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        if (Physics.BoxCast(transform.position, new Vector3(2f,2f, maxDist), transform.forward, out hit, transform.rotation, maxDist)){
+
+            if (hit.transform.tag == "Interactable") {
+
+                targetMat = hit.transform.gameObject.GetComponent<Renderer>().material;
+                targetMat.SetColor("ColorHdr", highlighColor);
+            }
+        }
+        
+        
+        
+            
+    }
+
+
+
+}
